a569c509d0f950d538435a9c23710502be2319dc
Stephen Kwon <mstorm@mstorm.net>
2019-06-02 22:26:31 +0900

    add calendar.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import ReactFullpage from '@fullpage/react-fullpage';
 
+const weeks = "일월화수목금토".split('');
+
 const App = () => (
     <ReactFullpage
         licenseKey="OPEN-SOURCE-GPLV3-LICENSE"
@@ -17,10 +19,11 @@ const App = () => (
                         <h1>초대합니다</h1>
                         <h2>민철과 소영</h2>
                         <p className="date">2019년 7월 6일 토요일 낮 12시</p>
-                        <p className="location"><a href="https://map.naver.com/local/siteview.nhn?code=11872992" rel="noopener noreferrer" target="_blank">분당앤스퀘어 4층 컨벤션홀</a></p>
+                        <p className="location"><a href="https://map.naver.com/local/siteview.nhn?code=11872992"
+                                                   rel="noopener noreferrer" target="_blank">분당앤스퀘어 4층 컨벤션홀</a></p>
                     </div>
                     <div className="section sec-welcome">
-                        <h1>인사말</h1>
+                        <h2>인사말</h2>
                         <p className="welcome">평생을 같이하고 싶은 사람을 만났습니다.<br/>
                             서로 아껴주고 이해하며<br/>
                             사랑 베풀며 살고 싶습니다.<br/>
@@ -31,12 +34,72 @@ const App = () => (
                             <li><strong>이윤식</strong>&middot;<strong>유율란</strong>의 장녀 <strong>소영</strong></li>
                         </ul>
                     </div>
+                    <div className="section sec-calendar">
+                        <h2>일정</h2>
+                        <p className="date">2019년 7월 6일 토요일 낮 12시</p>
+                        <p className="location"><a href="https://map.naver.com/local/siteview.nhn?code=11872992"
+                                                   rel="noopener noreferrer" target="_blank">분당앤스퀘어 4층 컨벤션홀</a></p>
+                        <table className="calendar">
+                            <thead>
+                            <tr>
+                                {weeks.map((v, i) => <th key={i}>{v}</th>)}
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td></td>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td><strong>6</strong></td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>8</td>
+                                <td>9</td>
+                                <td>10</td>
+                                <td>11</td>
+                                <td>12</td>
+                                <td>13</td>
+                            </tr>
+                            <tr>
+                                <td>14</td>
+                                <td>15</td>
+                                <td>16</td>
+                                <td>17</td>
+                                <td>18</td>
+                                <td>19</td>
+                                <td>20</td>
+                            </tr>
+                            <tr>
+                                <td>21</td>
+                                <td>22</td>
+                                <td>23</td>
+                                <td>24</td>
+                                <td>25</td>
+                                <td>26</td>
+                                <td>27</td>
+                            </tr>
+                            <tr>
+                                <td>28</td>
+                                <td>29</td>
+                                <td>30</td>
+                                <td>31</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                     <div className="section sec-location">
-                        <h1>오시는 길</h1>
-                        <a href="https://map.naver.com/local/siteview.nhn?code=11872992" rel="noopener noreferrer" target="_blank"><img src="/naver-map.png" alt="분당앤스퀘어" className="map" /></a>
+                        <h2>오시는 길</h2>
+                        <a href="https://map.naver.com/local/siteview.nhn?code=11872992" rel="noopener noreferrer"
+                           target="_blank"><img src="/naver-map.png" alt="분당앤스퀘어" className="map"/></a>
                         <div>
-                            <h3>분당앤스퀘어</h3>
-                            <p className="tel">031-728-5300</p>
+                            <p><strong>분당앤스퀘어</strong> <span className="tel">031-728-5300</span></p>
                             <p className="addr1">경기도 성남시 분당구 탄천상로151번길 20</p>
                             <p className="addr2">경기도 성남시 분당구 구미동 159</p>
                         </div>
